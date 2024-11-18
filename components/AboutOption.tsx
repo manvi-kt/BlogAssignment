f90faf352d5062bc7dcd1199896592d948303c4e
manvi-kt <manvi@kindtech.co.in>
2024-11-19 04:26:17 +0530

    Update AboutOption.tsx

--- components/AboutOption.tsx
+++ components/AboutOption.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { loginUser,logoutUser } from '@/slices/authSlice';
 import { useRouter } from 'next/router';
 import OptionList from './OptionList';
 import SubList from './Sublist';
 import { AboutDatas } from '@/data/data';
+import Sidebar from '@/components/Sidebar';
 import Header from '@/components/Header';
+import { RootState } from '../store';
+import { useDispatch , useSelector } from 'react-redux';
 
 const AboutLayout: React.FC = () => {
+  const dispatch = useDispatch();
+  const loggedIn = useSelector((state: RootState) => state.logged.loggedIn);
+  const handleLoginToggle = () => {
+    loggedIn ? dispatch(logoutUser()) : dispatch(loginUser());
+  };
   const router = useRouter();
   const { optionId, infoId } = router.query;
 
@@ -27,22 +36,7 @@ const AboutLayout: React.FC = () => {
     <div className=" w-full flex flex-col">
       <div className="flex ">
         <div className="w-1/5 fixed top-0 left-0 h-full">
-        <aside className="w-64 bg-white mt-10 text-blue-800 p-6 space-y-4 min-h-screen rounded-lg shadow-lg">
-      <nav className="space-y-2">
-        <a
-          href="/"
-          className=" mt-5 block py-2 px-4 rounded hover:bg-blue-100"
-        >
-          Home
-        </a>
-        <a
-          href="/about/"
-          className="block py-2 px-4 rounded hover:bg-blue-100"
-        >
-          About
-        </a>
-      </nav>
-    </aside>
+        <Sidebar loggedIn={loggedIn} handleLoginToggle={handleLoginToggle} />
         </div>
         <main className="ml-[20%] flex-1 max-w-4xl mx-auto mt-6 p-6">
           <div className="flex space-x-8">
