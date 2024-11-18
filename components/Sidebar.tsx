bd68ecc694770f9275e879458611fbf2d41c6387
manvi-kt <manvi@kindtech.co.in>
2024-11-19 04:27:05 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link';
 
 interface SidebarProps {
   loggedIn: boolean;
@@ -9,25 +10,18 @@ const Sidebar: React.FC<SidebarProps> = ({ loggedIn, handleLoginToggle }) => {
   return (
     <aside className="w-64 bg-white mt-10 text-blue-800 p-6 space-y-4 min-h-screen rounded-lg shadow-lg">
       <nav className="space-y-2">
-        <a
-          href="/"
-          className=" mt-5 block py-2 px-4 rounded hover:bg-blue-100"
-        >
+        <Link href="/" className="mt-5 block py-2 px-4 rounded hover:bg-blue-100">
           Home
-        </a>
-        <a
-          href="/about/"
-          className="block py-2 px-4 rounded hover:bg-blue-100"
-        >
+        </Link>
+        <Link href="/about/" className="block py-2 px-4 rounded hover:bg-blue-100">
           About
-        </a>
-        <a
-          href="#"
+        </Link>
+        <button
           onClick={handleLoginToggle}
-          className="block py-2 px-4 rounded hover:bg-blue-100"
+          className="block w-full text-left py-2 px-4 rounded hover:bg-blue-100"
         >
-          {loggedIn ? "Logout" : "Login"}
-        </a>
+          {loggedIn ? 'Logout' : 'Login'}
+        </button>
       </nav>
     </aside>
   );
